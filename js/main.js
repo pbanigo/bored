b004ae0d5c24eaf3e14548421842ec56a4b4bb47
Peter <41585899+pbanigo@users.noreply.github.com>
2022-04-08 15:37:46 +0100

    Added random fact

--- js/main.js
+++ js/main.js
@@ -8,29 +8,27 @@ function suggest(){
 	document.querySelector('p').style.display = 'none';	
 	document.querySelector('label').textContent	= 'Pick something else';
 	const url = `https://www.boredapi.com/api/activity/?type=${activity}&price=0`;
-
-
-
-
-
-
-
 	fetch(url, {mode: 'cors'})
     .then(res => res.json()) // parse response as JSON
     .then(data => {
     	document.querySelector('h1').textContent = `You can ${data.activity}`;
     	synth.speak(new SpeechSynthesisUtterance(document.querySelector('h1').textContent));//This reads any text in that element
-
-    	//img.src = data.message;
-    	//const base= data.drinks[0];
-    	console.log(url);
-    	console.log(data)
-    	//document.querySelector('title').textContent = `${base.strAlcoholic} Drink: How to make a ${base.strDrink}`;
-    	//document.querySelector('h2').textContent = base.strDrink;
-    	//document.querySelector('img').src = base.strDrinkThumb;
-    	//document.getElementById('instructions').textContent = base.strInstructions;
     })
     .catch(err => {
     	console.log(`error ${err}`)
     });
+
+//Generate Random Facts
+	const url2 = 'https://uselessfacts.jsph.pl/random.json?language=en';
+    fetch(url2, {mode: 'cors'})
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+    	console.log(data.text);
+    	document.getElementById('random').textContent = data.text;
+    	//document.querySelector('h1').textContent = `You can ${data.activity}`;
+    })
+    .catch(err => {
+    	console.log(`error ${err}`)
+    });
+    
 }
